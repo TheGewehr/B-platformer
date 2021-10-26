c2d9f4610a55c3430e246e0bfe82709de65548df
Carlos <arnau.carlos01@gmail.com>
2021-10-26 11:20:23 +0200

    a

---
+++ Game/Source/Physics.cpp
@@ -0,0 +1,12 @@
+#include "App.h"
+#include "Input.h"
+#include "Render.h"
+#include "Physics.h"
+#include "p2Point.h"
+#include "math.h"
+
+#ifdef _DEBUG
+#pragma comment( lib, "Box2D/libx86/Debug/Box2D.lib" )
+#else
+#pragma comment( lib, "Box2D/libx86/Release/Box2D.lib" )
+#endif
---
+++ Game/Source/Physics.h
@@ -0,0 +1,12 @@
+#include "App.h"
+
+#include "Box2D/Box2D/Box2D.h"
+
+#define GRAVITY_X 0.0f
+#define GRAVITY_Y -7.0f
+
+#define PIXELS_PER_METER 50.0f // if touched change METER_PER_PIXEL too
+#define METER_PER_PIXEL 0.02f // this is 1 / PIXELS_PER_METER !
+
+#define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
+#define PIXEL_TO_METERS(p)  ((float) METER_PER_PIXEL * p)
