24c0b246f456299f92e97d65154ec21fea1a91a4
Carlos <arnau.carlos01@gmail.com>
2021-10-28 13:22:00 +0200

    a

---
+++ Game/Source/Player.cpp
@@ -0,0 +1,47 @@
+#include "Player.h"
+#include "App.h"
+#include "Textures.h"
+#include "Input.h"
+#include "Render.h"
+#include "Audio.h"
+#include "Scene.h"
+#include "SDL/include/SDL_scancode.h"
+#include <stdio.h>
+#include <string>
+#include <math.h>
+
+//#include "Particles.h"
+//#include "Collisions.h"
+//#include "Enemies.h"
+//#include "Fonts.h"
+
+Player::Player(bool startEnabled) : Module() 
+{
+
+}
+
+Player::~Player() 
+{
+
+}
+
+bool Player::Update() 
+{
+
+}
+
+bool Player::PostUpdate()
+{
+
+}
+
+void Player::OnCollision(Collider* c1, Collider* c2) 
+{
+
+}
+
+
+void Player::SetAnimation(Animation &toChange) 
+{
+
+}
---
+++ Game/Source/Player.h
@@ -0,0 +1,114 @@
+#pragma once
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "App.h"
+#include "Module.h"
+#include "Animation.h"
+#include "Input.h"
+//#include "p2Point.h"
+
+#define PLAYER_LIFES_LEFT 3
+
+struct SDL_Texture;
+struct Collider;
+
+class Player : public Module
+{
+public:
+	// Constructor
+	Player(bool startEnabled);
+
+	// Destructor
+	~Player();
+
+	// Loads the necessary textures for the player
+	bool Start();
+
+	// Performs new input and handles player movement
+	bool Update();
+
+	//Performs the render call of the player sprite
+	bool PostUpdate();
+
+	void OnCollision(Collider* c1, Collider* c2);
+
+public:
+	// Position of the player in the map
+	fPoint position;
+
+	fPoint GetPlayerPosition() const 
+	{
+		return position;
+	}
+
+	// Camera
+	SDL_Rect camera;
+	int level;
+
+	// Margin of movement
+	int horizontalMargin = 1600;
+	int verticalMargin = 480;
+	
+	// The speed in which we move the player (pixels per frame)
+	int speed = 2;
+	int diagonalSpeed = 1;
+
+	//The player spritesheet loaded into an SDL_Texture
+	SDL_Texture* texture = nullptr;
+
+	// The pointer to the current player animation
+	// It will be switched depending on the player's movement direction
+	Animation* curentAnimation = nullptr;
+
+	// Set of animations
+	// IDLE animations
+	Animation rightIdleAnim;
+
+	// Move Animations
+	Animation rigthAnim;
+
+	// Damage Animatios
+	Animation hitFromRightAnim;
+
+	// Death Animation
+	Animation deathFromRightAnim;
+
+	void SetAnimation(Animation& toChange);
+
+	// A flag to detect when the player has been destroyed
+	bool destroyed = false;
+	 
+	int playerMaximumLife = 100;
+	int playerLife = playerMaximumLife;
+	int playerWidth = 10;
+	int playerHeight = 7;
+	int playerHeightOffset = 12;
+	int playerWidthOffset = 2;
+
+	// A countdown to when the player gets destroyed. After a while the game exits
+	uint exitCountdown = 120;
+
+	// Player keys
+	KeyState keyUp;
+	KeyState keyRight;
+	KeyState keyDown;
+	KeyState keyLeft;
+	KeyState keyJump;
+
+	KeyState lastHorizontalKey;
+	KeyState lastVerticalKey;
+	int lastDirection;
+
+	int iFrameTimerReference;
+	int iFrameTimer;
+	bool in_iFrame;
+
+	int canMoveCamera = true;
+
+	// Exit
+	int exit_counter;
+	SDL_Texture* exitTexture;
+};
+
+#endif //!__PLAYER_H__
